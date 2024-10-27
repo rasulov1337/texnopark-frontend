aa2947579b181b12fc1ffecdc560442bf77eef92
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2024-10-27 12:34:19 +0300

    Add search functionality (#4)
    Change content's lang meta tag to 'ru'

--- components/MainPage/MainPage.ts
+++ components/MainPage/MainPage.ts
@@ -1,10 +1,12 @@
 "use strict";
 
+import APIClient from "../../modules/ApiClient";
 import BookCard from "../BookCard/BookCard";
-import { BookCardData } from "../BookCard/BookCard";
+import { BookCardData } from "../../modules/Types";
 
 class MainPage {
     #templateContainer: HTMLDivElement;
+    #booksContainer: HTMLDivElement;
 
     constructor(books: BookCardData[]) {
         const template = Handlebars.templates["MainPage.hbs"];
@@ -12,14 +14,10 @@ class MainPage {
 
         this.#templateContainer.innerHTML = template({});
 
-        const booksContainer = this.#templateContainer.querySelector(
+        this.#booksContainer = this.#templateContainer.querySelector(
             ".js-books-container"
         ) as HTMLDivElement;
-        for (const book of books) {
-            const bookCard = new BookCard(book);
-            bookCard.render(booksContainer);
-            // booksContainer?.appendChild(bookCard);
-        }
+        this.#redrawCards(books);
 
         this.#addEventListeners();
     }
@@ -30,6 +28,34 @@ class MainPage {
             ?.addEventListener("click", () => {
                 console.log("I was clicked");
             });
+
+        this.#templateContainer
+            .querySelector(".js-search-form")
+            ?.addEventListener("submit", (e: Event) => {
+                e.preventDefault();
+                this.search();
+            });
+    }
+
+    async search() {
+        const search = this.#templateContainer.querySelector(
+            ".js-search"
+        ) as HTMLInputElement;
+        const booksData = await APIClient.getBooks({
+            startId: 0,
+            bookName: search.value,
+        });
+
+        this.#redrawCards(booksData);
+    }
+
+    async #redrawCards(booksData: BookCardData[]) {
+        this.#booksContainer.replaceChildren();
+
+        for (const bd of booksData) {
+            const bookCard = new BookCard(bd);
+            bookCard.render(this.#booksContainer);
+        }
     }
 
     render(parent: HTMLDivElement) {
