b9ed74f79e8e60e30aad70887b1ad0537fca2cad
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2024-10-27 15:29:09 +0300

    Add recommendations and bestsellers page

---
+++ components/BestsellersPage/BestsellersPage.ts
@@ -0,0 +1,67 @@
+"use strict";
+
+import APIClient from "../../modules/ApiClient";
+import BookCard from "../BookCard/BookCard";
+import { BookCardData } from "../../modules/Types";
+
+class BestsellersPage {
+    #templateContainer: HTMLDivElement;
+    #booksContainer: HTMLDivElement;
+
+    constructor(books: BookCardData[]) {
+        console.log("books", books);
+        const template = Handlebars.templates["BestsellersPage.hbs"];
+        this.#templateContainer = document.createElement("div");
+
+        this.#templateContainer.innerHTML = template(books);
+
+        this.#booksContainer = this.#templateContainer.querySelector(
+            ".js-books-container"
+        ) as HTMLDivElement;
+        this.#redrawCards(books);
+
+        this.#addEventListeners();
+    }
+
+    #addEventListeners() {
+        this.#templateContainer
+            .querySelector(".js-book-card")
+            ?.addEventListener("click", () => {
+                console.log("I was clicked");
+            });
+
+        this.#templateContainer
+            .querySelector(".js-search-form")
+            ?.addEventListener("submit", (e: Event) => {
+                e.preventDefault();
+                this.search();
+            });
+    }
+
+    async search() {
+        const search = this.#templateContainer.querySelector(
+            ".js-search"
+        ) as HTMLInputElement;
+        const booksData = await APIClient.getBooks({
+            startId: 0,
+            bookName: search.value,
+        });
+
+        this.#redrawCards(booksData);
+    }
+
+    async #redrawCards(booksData: BookCardData[]) {
+        this.#booksContainer.replaceChildren();
+
+        for (const bd of booksData) {
+            const bookCard = new BookCard(bd);
+            bookCard.render(this.#booksContainer);
+        }
+    }
+
+    render(parent: HTMLDivElement) {
+        parent.appendChild(this.#templateContainer);
+    }
+}
+
+export default BestsellersPage;
