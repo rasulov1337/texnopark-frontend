30c9eaf582e79e5518981136f2daa436a66bed6c
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2024-10-28 14:54:49 +0300

    Add deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite';
-import handlebarsCompilePlugin from './hbs-recompile.js';
+import { defineConfig } from "vite";
+import handlebarsCompilePlugin from "./hbs-recompile.js";
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
+    base: "/xakaton-frontend/",
     plugins: [handlebarsCompilePlugin()],
     server: {
-        host: 'localhost',
+        host: "localhost",
         port: 5173,
     },
     preview: {
-        host: '0.0.0.0',
+        host: "0.0.0.0",
         port: 5173,
     },
 });
